2297efcbd8a9a0a0e5d5d3e3290f9c406bd0bca4
serge <serge.goryachev@gmail.com>
2018-11-21 19:16:09 +0300

    usersController

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function show(){
+        $users = User::all(['name', 'email']);
+        return view('users')->with('users');
+    }
+}

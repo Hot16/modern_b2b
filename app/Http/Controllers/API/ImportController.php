6742c483b03946fda7861f52f831bca489cc1c59
serge <serge.goryachev@gmail.com>
2019-01-17 23:08:45 +0300

    чутка безопасности

--- app/Http/Controllers/API/ImportController.php
+++ app/Http/Controllers/API/ImportController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class ImportController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function import(){
         \App\Prices::truncate();
 

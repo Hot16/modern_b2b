d8b62a442f771379e6f2e11201793ed7c2a55b37
serge <serge.goryachev@gmail.com>
2019-03-21 19:01:55 +0300

    селект с 6 колонкой

--- resources/views/clients/clients_form.blade.php
+++ resources/views/clients/clients_form.blade.php
@@ -8,7 +8,7 @@
                 {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Введите имя']) !!}
                 {!! Form::text('login', null, ['class' => 'form-control', 'placeholder' => 'Введите логин'])!!}
                 {!! Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'Введите email'])!!}
-                {!! Form::select('price_level', ['0' => '0', '1' => '1', '2' => '2', '3' => '3', '4' => '4', '5' => '5'], null, ['class' => 'form-control']) !!}
+                {!! Form::select('price_level', ['0' => '0', '1' => '1', '2' => '2', '3' => '3', '4' => '4', '5' => '5', '6' => '6'], null, ['class' => 'form-control']) !!}
                 {!! Form::submit('Создать', ['class' => 'btn btn-outline-success']) !!}
                 {!! Form::close() !!}
             </div>
